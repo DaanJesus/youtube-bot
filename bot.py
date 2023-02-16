299f23c6fd066995ca5783c33e70c3ed92627878
PyTcheu <pedro.tcheu@gmail.com>
2023-02-15 22:56:47 -0300

    getting free proxysuser

--- bot.py
+++ bot.py
@@ -1,24 +1,34 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller # pip install chromedriver-autoinstaller
 
-import time
+chromedriver_autoinstaller.install() # To update your chromedriver automatically
+driver = webdriver.Chrome()
 
-#url = 'https://www.youtube.com/watch?v=k2eEyOZmi2c&ab_channel=CanalemDia'
-options = Options()
-options.add_experimental_option=True
+# Get free proxies for rotating
+def get_free_proxies(driver):
+    driver.get('https://sslproxies.org')
 
-webBrowser = webdriver.Chrome(options=options)
+    table = driver.find_element(By.TAG_NAME, 'table')
+    thead = table.find_element(By.TAG_NAME, 'thead').find_elements(By.TAG_NAME, 'th')
+    tbody = table.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
+
+    headers = []
+    for th in thead:
+        headers.append(th.text.strip())
+
+    proxies = []
+    for tr in tbody:
+        proxy_data = {}
+        tds = tr.find_elements(By.TAG_NAME, 'td')
+        for i in range(len(headers)):
+            proxy_data[headers[i]] = tds[i].text.strip()
+        proxies.append(proxy_data)
+    
+    return proxies
+
+
+free_proxies = get_free_proxies(driver)
+
+print(free_proxies)
 url = 'https://www.youtube.com/watch?v=k2eEyOZmi2c&ab_channel=CanalemDia'
-
-def open_new_tab(url, tab_number):
-    webBrowser.execute_script("window.open('about:blank', "+ str(tab_number) +");")
-    webBrowser.switch_to.window(str(tab_number) )
-    webBrowser.get(url)
-
-while True:
-    for i in range(10):
-        open_new_tab(url, i)
-
-    time.sleep(90)
-
-
